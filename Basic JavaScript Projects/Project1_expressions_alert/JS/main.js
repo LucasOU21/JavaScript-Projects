8de91ebf302946709bf893d1b4bd81991755ac5e
lulubit <lucasurrutia21@gmail.com>
2022-05-31 00:12:17 -0700

    Project1
    my first codes in Javascript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+//pop up window
+var W = 'This is day is pretty good'
+window.alert(W);
+
+var A = 'Hope everyone is having a good day!<br/>'
+document.write(A);
+//text methond
+document.write("Kip said \" Hello John! Good morning today\" and John said \" Good morning kip\" <br/> ")
+
+document.write("</br>\"People work better when they know what the goal is and why." 
++ " It is important that people look forward to coming to work in the morning and enjoy working.\""
++ "-Elon Musk <br/>");
+//Listed variables about Ferrari
+var cars = "The Ferrari", model_1 = "Ferrari Roma", model_2 = "Ferrari 812 GTS", model_3 = "<br/> Ferrai SF90", model_4 = "Ferrari 812 superfast"
+document.write(model_3); 
+
+//code turned font color to yellow
+var Yellow = "<br/><br/>I got yellow fever"
+var Yellow = Yellow.fontcolor("yellow");
+document.write(Yellow);
+//expression
+document.write("<br/>yellow + green");
