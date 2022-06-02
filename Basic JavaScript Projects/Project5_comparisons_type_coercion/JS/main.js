ac55251749004900ebc5cfeeb70afbc2025747b3
lulubit <lucasurrutia21@gmail.com>
2022-06-01 20:33:23 -0700

    Project5 Comparisons and coercion
    Used Comparisons and coercion operators in JavaScript

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,55 @@
+// This is the code from assigment 26-
+
+// document.write(typeof "tiger");
+// document.write(typeof "10");
+// document.write(8E324);
+// document.write(-10E343);
+// document.write(20 < 21)
+// document.write(100 > 200)
+
+
+//consolelog view on devtools
+var Name = "Lulu";
+console.log("hello you can call me " + Name);
+
+
+
+//coercion
+
+//document.write("-23" + 21);
+//document.write("10000000" + 200);
+
+
+var wrong = "21<46"
+console.log("This statement is false " + wrong)
+
+
+// Double equal signs operation
+//document.write(120== 120);
+//document.write(82== 83)
+
+//Triple equal Signs
+D = 21;
+E = 21;
+
+F = 34;
+G = "82";
+
+H = "Burger";
+I = "Burger";
+
+// The AND operator
+// document.write(21 < 13 && 13 > 12);
+// The OR operator
+// document.write(8 > 90 || 50 > 30);
+
+// The NOT operator
+function not_Function() {
+    document.getElementById("Not") .innerHTML = !(1000 > 100)
+
+}
+function not_Function() {
+    document.getElementById("Not_F") .innerHTML = !(100 > 1000)
+
+}
+
